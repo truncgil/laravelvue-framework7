841ff6e1bf1b2d526471ded72df50cf764536b02
Ümit Tunç <umitreva@gmail.com>
2022-12-01 14:28:51 +0300

    add sample store data

--- framework7/src/js/store.js
+++ framework7/src/js/store.js
@@ -7,16 +7,139 @@ const store = createStore({
       {
         id: '1',
         title: 'Apple iPhone 8',
+        price : '1290.90 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
       },
       {
         id: '2',
         title: 'Apple iPhone 8 Plus',
+        price : '1599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
         description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
       },
       {
         id: '3',
         title: 'Apple iPhone X',
+        price : '2599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+      },
+      {
+        id: '1',
+        title: 'Apple iPhone 8',
+        price : '1290.90 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+      },
+      {
+        id: '2',
+        title: 'Apple iPhone 8 Plus',
+        price : '1599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+      },
+      {
+        id: '3',
+        title: 'Apple iPhone X',
+        price : '2599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+      },
+      {
+        id: '1',
+        title: 'Apple iPhone 8',
+        price : '1290.90 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+      },
+      {
+        id: '2',
+        title: 'Apple iPhone 8 Plus',
+        price : '1599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+      },
+      {
+        id: '3',
+        title: 'Apple iPhone X',
+        price : '2599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+      },
+      {
+        id: '1',
+        title: 'Apple iPhone 8',
+        price : '1290.90 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+      },
+      {
+        id: '2',
+        title: 'Apple iPhone 8 Plus',
+        price : '1599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+      },
+      {
+        id: '3',
+        title: 'Apple iPhone X',
+        price : '2599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+      },
+      {
+        id: '1',
+        title: 'Apple iPhone 8',
+        price : '1290.90 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+      },
+      {
+        id: '2',
+        title: 'Apple iPhone 8 Plus',
+        price : '1599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+      },
+      {
+        id: '3',
+        title: 'Apple iPhone X',
+        price : '2599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+      },
+      {
+        id: '1',
+        title: 'Apple iPhone 8',
+        price : '1290.90 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+      },
+      {
+        id: '2',
+        title: 'Apple iPhone 8 Plus',
+        price : '1599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
+        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+      },
+      {
+        id: '3',
+        title: 'Apple iPhone X',
+        price : '2599.99 $',
+        picture: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4de62b68299019.5b603c2a5a02b.jpg',
+
         description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
       },
     ]
